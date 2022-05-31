a8e051994db542ee6cf0e31df1f5ec493bb3bb54
olmax99 <olmighty99@gmail.com>
2022-05-31 17:55:14 +0200

    feat: comments, go new generic types -wip

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/TheAlgorithms/Go
 
+replace github.com/TheAlgorithms/Go => github.com/olmax99/Go v0.0.3-alpha.0.20220531155514-e863a4f1506c
+
 go 1.18
--- math/power/fastexponent.go
+++ math/power/fastexponent.go
@@ -1,21 +1,25 @@
 package power
 
+import "github.com/TheAlgorithms/Go/constraints"
+
 // IterativePower is iterative O(logn) function for pow(x, y)
-func IterativePower(n uint, power uint) uint {
-	var res uint = 1
+func IterativePower[T constraints.Integer](n, power T) T {
+	var res T = 1
+
 	for power > 0 {
+		// even => 0, odd => 1, calculate only odd
 		if (power & 1) != 0 {
 			res = res * n
 		}
 
-		power = power >> 1
-		n *= n
+		power = power >> 1 // modulus, odd number => odd result, 1 >> 1 yields 0
+		n *= n             // multiply with itself
 	}
 	return res
 }
 
 // RecursivePower is recursive O(logn) function for pow(x, y)
-func RecursivePower(n uint, power uint) uint {
+func RecursivePower[T constraints.Integer](n, power T) T {
 	if power == 0 {
 		return 1
 	}
@@ -27,7 +31,7 @@ func RecursivePower(n uint, power uint) uint {
 }
 
 // RecursivePower1 is recursive O(n) function for pow(x, y)
-func RecursivePower1(n uint, power uint) uint {
+func RecursivePower1[T constraints.Integer](n, power T) T {
 	if power == 0 {
 		return 1
 	} else if power%2 == 0 {
